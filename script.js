c1a57f98e99d0d3055a3a0dbc206bc9afbb78815
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2024-04-10 00:44:11 -0500

    nsf

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
-document.querySelector("#abc").addEventListener("input", function(e) {
-  const len = e.target.value.length / 2;
-  const letter = String.fromCharCode(len + 65);
+function visibleLength(str) {
+  let count = 0;
+  let arr = [...str];
 
-  if(len > 25) {
-    document.querySelector("#abcLabel").innerHTML = "Alphabet"
-  } else {
-    
-  document.querySelector("#abcLabel").innerText = `Type ${letter}`
+  for (c = 0; c < arr.length; c++) {
+      if (
+          arr[c] != '\u{200D}' &&
+          arr[c + 1] != '\u{200D}' &&
+          arr[c + 1] != '\u{fe0f}' &&
+          arr[c + 1] != '\u{20e3}'
+      ) {
+          count++;
+      }
+  }
+  return count;
+}
+
+for(let i = 0; i < 26; i++) {
+const myI = i;
+  const input = document.createElement("input");
+input.id = `abc${myI}`;
+input.placeholder = `Type ${String.fromCharCode(65 + myI)}`;
+input.addEventListener("input", (e) => {
+  const nextInput = document.getElementById(`abc${myI + 1}`);
+
+  if (nextInput) {
+    nextInput.focus();
   }
 })
 
+document.querySelector("#abcInputs").appendChild(input)
+
+}
+
 document.querySelector("#submit").addEventListener("click", function(e) {
-  e.preventDefault();
-  const text = document.querySelector("#input").value.toUpperCase();
-  const dict = [...(document.querySelector("#abc").value)];
+e.preventDefault();
+const text = document.querySelector("#input").value.toUpperCase();
+const abc = Array.from(document.querySelectorAll("#abcInputs input")).map((a) => a.value);
+console.log(abc);
 
-  const result = text.split("").map(char => {
-    const code = char.charCodeAt(0);
-    const index = code - 65;
+const result = text.split("").map(char => {
+  const code = char.charCodeAt(0);
+  const index = code - 65;
 
-    if(index < 0 || index > 25) {
-      return char;
-    }
-    
-    const letter = dict[index];
+  if(index < 0 || index > 25) {
+    return char;
+  }
+  
+  const letter = abc[index];
 
-    return letter;
-    
-  }).join("");
+  return letter;
+  
+}).join("");
 
-  document.querySelector("#input").value = result;
+document.querySelector("#input").value = result;
 })
