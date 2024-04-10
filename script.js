26bb1f0cd4651d6241568310d2757271fbbe49d4
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2024-04-10 00:03:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelector("#abc").addEventListener("input", function(e) {
+  const len = e.target.value.length;
+  const letter = String.fromCharCode(len + 65);
+
+  if(len > 25) {
+    document.querySelector("#abcLabel").innerHTML = "Alphabet"
+  } else {
+    
+  document.querySelector("#abcLabel").innerText = `Type ${letter}`
+  }
+})
+
+document.querySelector("#submit").addEventListener("click", function(e) {
+  e.preventDefault();
+  const text = document.querySelector("#input").value.toUpperCase();
+  const dict = document.querySelector("#abc").value.split("");
+
+  const result = text.split("").map(char => {
+    const code = char.charCodeAt(0);
+    const index = code - 65;
+
+    if(index < 0 || index > 25) {
+      return char;
+    }
+    
+    const letter = dict[index];
+
+    return letter;
+    
+  }).join("");
+
+  document.querySelector("#input").value = result;
+})
